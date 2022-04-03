9b0058ef576089080b33481105c337fa9d52eac4
Vlad <taguerrancompany@gmail.com>
2022-04-03 02:18:00 -0300

    Adição de properties, configurando ambiente de desenvolvimento e produção.

--- src/main/java/io/github/VladmirTaguerran/VendasApplication.java
+++ src/main/java/io/github/VladmirTaguerran/VendasApplication.java
@@ -1,6 +1,7 @@
 package io.github.VladmirTaguerran;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
+    @Value("${application.name}")
     private String applicationName;
 
 
--- src/main/java/io/github/VladmirTaguerran/WebConfiguration.java
+++ src/main/java/io/github/VladmirTaguerran/WebConfiguration.java
@@ -1,14 +1,19 @@
 package io.github.VladmirTaguerran;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class WebConfiguration {
 
-    @Bean(name = "applicationName")
-    public  String applicationName(){
-        return  "Sistemas de vendas";
-    }
+   @Bean
+    public CommandLineRunner exec(){
+       return args -> {
+           System.out.println("Rodando Sistema em Desenvolvimento");
+       };
+   }
 
 }
