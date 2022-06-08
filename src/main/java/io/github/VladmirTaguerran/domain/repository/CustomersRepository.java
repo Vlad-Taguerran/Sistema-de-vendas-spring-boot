0724bf8882988a40f967facc8e48106211b163ff
Vlad <taguerrancompany@gmail.com>
2022-06-08 00:15:01 -0300

    Usando interface JpaRepository

--- src/main/java/io/github/VladmirTaguerran/domain/repository/CustomersRepository.java
+++ src/main/java/io/github/VladmirTaguerran/domain/repository/CustomersRepository.java
@@ -5,8 +5,12 @@ import io.github.VladmirTaguerran.domain.entity.Customers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+
+        List<Customers> findByNameLike(String name);
 
 
 
